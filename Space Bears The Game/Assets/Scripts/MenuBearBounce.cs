8fa6d45fd9e306606b130368378dab73d100ce36
AIsDaBoss <139798604+AIsDaBoss@users.noreply.github.com>
2023-07-19 15:31:19 -0400

    menu update

--- Space Bears The Game/Assets/Scripts/MenuBearBounce.cs
+++ Space Bears The Game/Assets/Scripts/MenuBearBounce.cs
@@ -9,19 +9,40 @@ public class MenuBearBounce : MonoBehaviour
     public float rotationSpeed;
     public Rigidbody rb;
 
+    public bool move = false;
+    public bool moved = false;
+    public float cd = 1;
+
     // Start is called before the first frame update
     void Start()
     {
         Physics.bounceThreshold = 0;
-
-        rb.AddForce(new Vector3(Random.Range(-1, 1), Random.Range(-1, 1), Random.Range(-1, 1)).normalized * speed, ForceMode.Impulse);
-        rb.AddTorque(0, 0, Random.Range(-rotationSpeed,rotationSpeed));
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
         rb.velocity = rb.velocity.normalized * speed;
+
+        if (move && !moved)
+        {
+            moved = true;
+
+            rb.AddForce(new Vector3(1, 1, 1).normalized * speed, ForceMode.Impulse);
+            rb.AddTorque(0, 0, Random.Range(-rotationSpeed, rotationSpeed));
+        }
+        else
+        {
+            if(!moved)
+            {
+                cd -= Time.deltaTime;
+            }
+        }
+
+        if (cd <= 0)
+        {
+            move = true;
+        }
     }
 
     private void OnMouseDown()
