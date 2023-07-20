d893396c231744f893ad21af5921c35af425d07d
GradyPom <139798712+GradyPom@users.noreply.github.com>
2023-07-20 10:51:11 -0400

    UPDATES

--- Space Bears The Game/Assets/Scripts/LaserMove.cs
+++ Space Bears The Game/Assets/Scripts/LaserMove.cs
@@ -7,12 +7,16 @@ public class LaserMove : MonoBehaviour
     public float speed = 30;
     public float laserStrength;
     public float laserCooldown;
+    public AudioClip laserSound;
+    private AudioSource audioSource;
 
     // Start is called before the first frame update
     void Start()
     {
+        audioSource = GetComponent<AudioSource>();
         laserStrength = 10;
         laserCooldown = 1;
+        audioSource.PlayOneShot(laserSound, 1);
     }
 
     // Update is called once per frame
